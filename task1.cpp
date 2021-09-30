82dd2ac421664ed1ff3375dd9c81cdc26e058c0e
WITCHERBLADE <87032590+WITCHERBLADE@users.noreply.github.com>
2021-09-30 10:35:57 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+void charfunction()
+   {
+    char a='A';
+cout<<a<<endl;
+   }
+void integerfunction()
+   {
+    int a=200;
+cout<<a<<endl;
+   }
+void floatfunction()
+   {
+    float a=11.111111111;
+cout<<a<<endl;
+   }
+void doublefunction()
+   {
+    double a=1.11111111;
+cout<<a<<endl;
+   }
+void bollianfunction()
+   {
+    bool a=-1;
+cout<<a<<endl;
+   }
+void shortfunction()
+   {
+    short a=23;
+cout<<a<<endl;
+   }
+void longfunction()
+   {
+    long a=111111111;
+cout<<a<<endl;
+   }
+void longdoublefunction()
+   {
+    long double a=1.111111111;
+cout<<a<<endl;
+   }
+void widefunction()
+   {
+    wchar_t a=L'A';
+cout<<a<<endl;
+   }
+
+
+int main()
+{
+charfunction();
+integerfunction();
+floatfunction();
+ doublefunction();
+bollianfunction();
+shortfunction();
+longfunction();
+longdoublefunction();
+widefunction();
+
+
+return 0;
+}
+
+    
